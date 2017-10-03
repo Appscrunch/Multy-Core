5c8f9924f43374046b71e3cbc5827dd827ad671b
Vasily Nemkov <V.Nemkov@gmail.com>
2017-10-03 18:08:48 +0300

    Moved some usefult code to the internal/utility.h

--- internal/utility.h
+++ internal/utility.h
@@ -1,4 +1,77 @@
-#ifndef UTILITY_H
-#define UTILITY_H
+#ifndef WALLET_UTILITY_H
+#define WALLET_UTILITY_H
+
+#include "error.h"
+
+#include <memory>
+
+struct Error;
+
+namespace wallet_core
+{
+
+namespace internal
+{
+/** Convenience to simplify passing C++ smart_pointers (like std::unique_ptr<T>)
+ * to C-like functions than take T** and store address of new object there.
+ * Should be used in conjunction with reset_sp() function.
+ * Resets value of smart pointer if it was requested to be converted to pointer-to-pointer (T**)
+ * and that value was modified during lifetime of the object.
+ */
+template <typename SP>
+class UniquePointerUpdater
+{
+    typedef typename SP::pointer Pointer;
+    SP& sp;
+    mutable Pointer p;
+public:
+    explicit UniquePointerUpdater(SP& sp)
+        : sp(sp),
+          p(sp.get())
+    {}
+    ~UniquePointerUpdater()
+    {
+        if (p != sp.get())
+        {
+            sp.reset(p);
+        }
+    }
+    operator Pointer*() const
+    {
+        p = sp.get();
+        return &p;
+    }
+};
+
+/// See UniquePointerUpdater.
+template <typename SP>
+UniquePointerUpdater<SP> reset_sp(SP& sp)
+{
+    return UniquePointerUpdater<SP>(sp);
+}
+
+template <typename T, typename D>
+std::unique_ptr<T, D> null_unique_ptr(D deleter)
+{
+    return std::unique_ptr<T, D>(nullptr, deleter);
+}
+
+void throw_if_error(Error* err)
+{
+    if (err)
+    {
+        throw err;
+    }
+}
+
+} // namespace internal
+
+} // namespace wallet_core
+
+#define ARG_CHECK(arg) do {\
+    if (!(arg)) {\
+        return make_error(ERROR_INVALID_ARGUMENT, "Argument check failed: " #arg); \
+    }\
+} while (false)
 
 #endif // UTILITY_H
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "common.h"
 #include "error.h"
+#include "internal/utility.h"
 
 #include <string.h>
 #include <iostream>
@@ -30,63 +31,7 @@ size_t entropySource(size_t size, void* dest)
     return size;
 }
 
-void throw_if_error(Error* err)
-{
-    if (err)
-    {
-        throw err;
-    }
-}
-
-template <typename T, typename D>
-std::unique_ptr<T, D> up(T* ptr, D deleter)
-{
-    return std::unique_ptr<T, D>(ptr, deleter);
-}
-
-/** Convenience to simplify passing C++ smart_pointers (like std::unique_ptr<T>)
- * to C-like functions than take T** and store address of new object there.
- * Should be used in conjunction with reset_sp() function.
- * Resets value of smart pointer if it was requested to be converted to pointer-to-pointer (T**)
- * and that value was modified during lifetime of the object.
- */
-template <typename SP>
-class UniquePointerUpdater
-{
-    typedef typename SP::pointer Pointer;
-    SP& sp;
-    mutable Pointer p;
-public:
-    explicit UniquePointerUpdater(SP& sp)
-        : sp(sp),
-          p(sp.get())
-    {}
-    ~UniquePointerUpdater()
-    {
-        if (p != sp.get())
-        {
-            sp.reset(p);
-        }
-    }
-    operator Pointer*() const
-    {
-        p = sp.get();
-        return &p;
-    }
-};
-
-/// See UniquePointerUpdater.
-template <typename SP>
-UniquePointerUpdater<SP> reset_sp(SP& sp)
-{
-    return UniquePointerUpdater<SP>(sp);
-}
-
-template <typename T, typename D>
-std::unique_ptr<T, D> null_unique_ptr(D deleter)
-{
-    return std::unique_ptr<T, D>(nullptr, deleter);
-}
+using namespace wallet_core::internal;
 
 } // namespace
 
